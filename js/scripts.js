2b67a6fab144bb4c1ab618bf463fa037b23067ce
eondula <leezmiriam@gmail.com>
2023-09-23 16:35:31 -0700

    edited and added tools

--- js/scripts.js
+++ js/scripts.js
@@ -11,19 +11,19 @@ document.addEventListener("DOMContentLoaded", function() {
             </p>
 
             <p>
-            Throughout her journey, she has facilitated various workshops and boot camps, focusing on areas such as robotics, programming, and embedded systems. Her roles in different communities have been diverse, from being a Taskforce Member at the Ministry of ICT, Innovation, and Youth Affairs in Kenya, an Artifact Evaluation Committee Member for SuperComputing 2021, and an Artifact Evaluation Board member. At USC, she serves as the Director of Civic Engagement for the Black Graduate Student Network and is an Organizing Committee member of the Ph.D. Women and Gender Minorities Computing Club. She has also mentored in various capacities, including at the Viterbi Summer Institute, the Women in Engineering program, the Viterbi Mentorship Program (at USC), IBM Bluehack, Tech Avenue at TUK, Foondi Workshops, and Women Who Mentor and Innovate in Africa (now Women in Tech KE).
+            Throughout her journey, she has facilitated various workshops and boot camps, focusing on areas such as robotics, programming, and embedded systems. Her roles in different communities have been diverse, from being a Taskforce Member at the Ministry of ICT, Innovation, and Youth Affairs in Kenya, an Artifact Evaluation Committee Member for SuperComputing 2021, and an Artifact Evaluation Board member for Journal of Systems Research. At USC, she serves as the Director of Civic Engagement for the Black Graduate Student Network and is an Organizing Committee member of the Ph.D. Women and Gender Minorities Computing Club. She has also mentored in various capacities, including at the Viterbi Summer Institute, the Women in Engineering program, the Viterbi Mentorship Program (at USC), IBM Bluehack, Tech Avenue at TUK, Foondi Workshops, and Women Who Mentor and Innovate in Africa (now Women in Tech KE).
             </p>
 
             <p>
-            She has been recognized with several awards, including the Grace Hopper Celebration Travel Grant, OSDI ’18 Diversity Grant, and Annenberg Fellowship, and was acknowledged as the Outstanding Alumni Class of 2014 at the Technical University of Kenya.
+            She has been recognized with a couple of awards including the Grace Hopper Celebration Travel Grant, OSDI ’18 Diversity Grant, and Annenberg Fellowship, and was acknowledged as the Outstanding Alumni Class of 2014 at the Technical University of Kenya.
             </p>
 
             <p>
-            Elizabeth's research is in reinforcement learning, specifically its application to scenarios for optimizing decision-making strategies under uncertainties. Her current work integrates stochastic epidemic modeling with reinforcement learning, addressing complexities in educational settings during epidemics. Apart from reinforcement learning, she delves into intelligent autonomy, human-AI interaction, multi-agent systems, and AI tool design and development.
+            Elizabeth's research is in reinforcement learning, specifically its application to scenarios for optimizing decision-making strategies under uncertainties. Her current work integrates stochastic epidemic modeling with reinforcement learning, addressing complexities in educational settings during epidemics. Apart from reinforcement learning, she delves into intelligent autonomy, robotics,  human-AI interaction, multi-agent systems, and AI tool design and development.
             </p>
 
             <p>
-            Beyond academic and professional pursuits, she is loves live music, spoken word, poetry, photography, and outdoor activities.
+            Beyond academic and professional pursuits, she is loves live music, spoken word, poetry, fine arts, photography and outdoor activities.
             </p>
             `;
                 break;
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 break;
 
             case 'tools':
-                container.innerHTML = "<h>Check out github (personal and ANRG)</h>";
+                container.innerHTML = document.getElementById('tools-list').innerHTML;
                 break;
             default:
                 container.innerHTML = "<p>Select a content type.</p>";
@@ -93,9 +93,10 @@ document.addEventListener("DOMContentLoaded", function() {
         showContent('thoughts');
     });
 
-    document.getElementById("projBtn").addEventListener("click", function() {
+    document.getElementById("toolsBtn").addEventListener("click", function() {
         showContent('tools');
     });
+
 
 
 
