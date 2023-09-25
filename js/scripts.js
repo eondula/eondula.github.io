1ccd4edd225f66107fbfa7a4c4aa1fec8b062d3c
eondula <leezmiriam@gmail.com>
2023-09-25 14:17:04 -0700

    updated bio

--- js/scripts.js
+++ js/scripts.js
@@ -7,24 +7,33 @@ document.addEventListener("DOMContentLoaded", function() {
             case 'bio':
                 container.innerHTML = `
             <p>
-            Elizabeth is a student of Computer Science at the <a href="https://www.usc.edu/" target="_blank">University of Southern California</a>. She is a member of the <a href="https://anrg.usc.edu/" target="_blank">Autonomous Networks Research Group</a>, with Prof. Bhaskar Krishnamachari as her faculty advisor and mentor. Before joining USC, she worked with IBM Research in Africa, Brave Venture Labs, and iHub Nairobi. She received her Bachelor of Engineering in Electrical and Electronics Engineering from the <a href="https://www.tukenya.ac.ke/" target="_blank">Technical University of Kenya</a>. 
-            </p>
+            Elizabeth is a student of Computer Science at the <a href="https://www.usc.edu/" target="_blank">University of Southern California</a>. She is a member of the <a href="https://anrg.usc.edu/" target="_blank">Autonomous Networks Research Group</a>, with Prof. Bhaskar Krishnamachari as her faculty advisor and mentor. Before joining USC, she worked with IBM Research in Africa, Brave Venture Labs, and iHub Nairobi. She received her Bachelor of Engineering in Electrical and Electronics Engineering from the <a href="https://www.tukenya.ac.ke/" target="_blank">Technical University of Kenya</a>.
+</p>
 
-            <p>
-            Throughout her journey, she has facilitated various workshops and boot camps, focusing on areas such as robotics, programming, and embedded systems. Her roles in different communities have been diverse, from being a Taskforce Member at the Ministry of ICT, Innovation, and Youth Affairs in Kenya, an Artifact Evaluation Committee Member for SuperComputing 2021, and an Artifact Evaluation Board member for Journal of Systems Research. At USC, she serves as the Director of Civic Engagement for the Black Graduate Student Network and is an Organizing Committee member of the Ph.D. Women and Gender Minorities Computing Club. She has also mentored in various capacities, including at the Viterbi Summer Institute, the Women in Engineering program, the Viterbi Mentorship Program (at USC), IBM Bluehack, Tech Avenue at TUK, Foondi Workshops, and Women Who Mentor and Innovate in Africa (now Women in Tech KE).
-            </p>
+<p>
+            Throughout her journey, she has facilitated various workshops and boot camps, focusing on areas such as robotics, programming, and embedded systems. Her roles in different communities have been diverse, from being a Taskforce Member at the Ministry of ICT, Innovation, and Youth Affairs in Kenya, an Artifact Evaluation Committee Member for SuperComputing 2021, and an Artifact Evaluation Board member for Journal of Systems Research. At USC, she serves as the Director of Civic Engagement for the Black Graduate Student Network and is an Organizing Committee member of the Ph.D. Women and Gender Minorities Computing Club.
+</p>
 
-            <p>
-            She has been recognized with a couple of awards including the Grace Hopper Celebration Travel Grant, OSDI ’18 Diversity Grant, and Annenberg Fellowship, and was acknowledged as the Outstanding Alumni Class of 2014 at the Technical University of Kenya.
-            </p>
+<p>
+            Elizabeth initiated and nurtured robotics clubs and played a crucial role in launching kids hacker camps, for which she was recognized in The 100: Tech Community Appreciation Awards. Her endeavor in these initiatives was paramount in fostering knowledge and passion in robotics and technology among the youth.
+</p>
 
-            <p>
-            Elizabeth's research is in reinforcement learning, specifically its application to scenarios for optimizing decision-making strategies under uncertainties. Her current work integrates stochastic epidemic modeling with reinforcement learning, addressing complexities in educational settings during epidemics. Apart from reinforcement learning, she delves into intelligent autonomy, robotics,  human-AI interaction, multi-agent systems, and AI tool design and development.
-            </p>
+<p>
+            She has also mentored in various capacities, including at the Viterbi Summer Institute, the Women in Engineering program, the Viterbi Mentorship Program (at USC), IBM Bluehack, Tech Avenue at TUK, Foondi Workshops, and Women Who Mentor and Innovate in Africa (now Women in Tech KE).
+</p>
 
-            <p>
-            Beyond academic and professional pursuits, she is loves live music, spoken word, poetry, fine arts, photography and outdoor activities.
-            </p>
+<p>
+            Elizabeth has been honored with several awards and recognitions, including the Grace Hopper Celebration Travel Grant, OSDI ’18 Diversity Grant, and Annenberg Fellowship. She was also acknowledged as the Outstanding Alumni Class of 2014 at the Technical University of Kenya.
+</p>
+
+<p>
+            Her research focuses on reinforcement learning, particularly its application to scenarios for optimizing decision-making strategies under uncertainties. Her current work integrates stochastic epidemic modeling with reinforcement learning to address complexities in educational settings during epidemics. Her areas of interest also include the mathematics of intelligent autonomy, robotics, human-AI interaction, multi-agent systems, and AI tool design and development.
+</p>
+
+<p>
+            Beyond academic and professional pursuits, Elizabeth loves live music, spoken word, poetry, fine arts, photography, and outdoor activities.
+</p>
+
             `;
                 break;
 
