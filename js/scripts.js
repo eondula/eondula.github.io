3229ca3b7147fd950148d3b34c357c0ee0620850
eondula <leezmiriam@gmail.com>
2023-12-30 19:15:25 -0800

    changed layout and bio

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
             case 'bio':
                 container.innerHTML = `
             <p>
-            Elizabeth is a student of Computer Science at the <a href="https://www.usc.edu/" target="_blank">University of Southern California</a>. She is a member of the <a href="https://anrg.usc.edu/" target="_blank">Autonomous Networks Research Group</a>, with Prof. Bhaskar Krishnamachari as her faculty advisor and mentor.
-            Her research focuses on reinforcement learning and its applications in guiding informed decision-making under uncertainties. Her current work integrates stochastic epidemic modeling with reinforcement learning to address operational problems in spaces during epidemics. Other areas of interest also include the mathematics of intelligent autonomy, robotics, human-AI interaction, multi-agent systems, and AI tool design and development.
-
+             Elizabeth (Leez) is a Ph.D student of Computer Science at the <a href="https://www.usc.edu/" target="_blank">University of Southern California</a>. She is a member of the <a href="https://anrg.usc.edu/" target="_blank">Autonomous Networks Research Group</a>, with <a href="https://ceng.usc.edu/~bkrishna/" target="_blank">Prof Bhaskar Krishnamachari</a>.  as her faculty advisor and mentor.
+            My research interests are in reinforcement learning, stochastic modeling, decision science, optimization, robotics, AI tool design and Human-AI interaction
             </p>
-
+<!--            <p>-->
+<!--            Throughout her academic journey, Elizabeth has organized and participated in various workshops and boot camps, emphasizing areas like robotics, programming, and embedded systems. She has been involved in numerous communities, holding positions such as a Taskforce Member at the Ministry of Information, Communication, and the Digital Economy in Kenya, an Artifact Evaluation Committee Member for SuperComputing 2021, and an Artifact Evaluation Board member for the Journal of Systems Research. Within the University of Southern California, she undertakes the role of Director of Civic Engagement for the Black Graduate Student Network and an  Organizing Committee member for the Ph.D. Women and Gender Minorities Computing Club. She is passionate about STEAM and education mentorship for the next generation, fostering knowledge and enthusiasm among individuals at any level.-->
+<!--            </p>-->
 <p>
-            Throughout her academic journey, Elizabeth has organized and participated in various workshops and boot camps, emphasizing areas like robotics, programming, and embedded systems. She has been involved in numerous communities, holding positions such as a Taskforce Member at the Ministry of Information, Communication, and the Digital Economy in Kenya, an Artifact Evaluation Committee Member for SuperComputing 2021, and an Artifact Evaluation Board member for the Journal of Systems Research. Within the University of Southern California, she undertakes the role of Director of Civic Engagement for the Black Graduate Student Network and an  Organizing Committee member for the Ph.D. Women and Gender Minorities Computing Club. She is passionate about STEAM and education mentorship for the next generation, fostering knowledge and enthusiasm among individuals at any level.
-
-</p>
-
-
-<p>
- Before her Ph.D. journey at USC, Elizabeth worked with IBM Research, Brave Venture Labs, and iHub Nairobi. Her foundational academic background is a Bachelor of Engineering in Electrical and Electronics Engineering, which she obtained from the Technical University of Kenya. In her personal time, Elizabeth finds solace in music, spoken word, poetry, fine arts, and mural photography, and journaling. She also appreciates the serenity and challenges of outdoor activities.
+ Prior to starting her Ph.D. journey at USC, Elizabeth worked as a <strong>Software Engineer</strong> at IBM Research, <strong> Head of Product Development</strong> at Brave Venture Labs, and <strong> Co-lead of Hardware Research </strong> at iHub Nairobi. Her foundational academic background is a Bachelor of Engineering in Electrical and Electronics Engineering, which she obtained from the Technical University of Kenya. In her personal time, Elizabeth finds solace in music, spoken word, poetry, fine arts, and mural photography, and journaling. She also appreciates the serenity of outdoor activities.
 
 </p>
 
@@ -94,8 +89,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("toolsBtn").addEventListener("click", function() {
         showContent('tools');
     });
-
-
 
 
 });
